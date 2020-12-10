6283d3d8f2f86084053658d5c219b17db7ac528d
Steve Ellis <email@steveell.is>
2020-12-10 00:17:52 -0500

    consolidate tests into Registry file

--- test/Registry_test.js
+++ test/Registry_test.js
@@ -16,6 +16,7 @@ contract('Registry', (accounts) => {
   const executeGas = new BN('100000')
   const emptyBytes = '0x00'
   const rewardCallers = new BN('3')
+  const extraGas = new BN('250000')
 
   beforeEach(async () => {
     LinkToken.setProvider(web3.currentProvider)
@@ -34,9 +35,19 @@ contract('Registry', (accounts) => {
     await this.link.transfer(user1, ether('100'), { from: maintainer })
     await this.link.transfer(user2, ether('100'), { from: maintainer })
     await this.link.transfer(user3, ether('100'), { from: maintainer })
+
+    const { receipt } = await this.registry.addJob(
+      this.dummy.address,
+      executeGas,
+      rewardCallers,
+      emptyBytes,
+      { from: user1 }
+    )
+    const executorAddr = receipt.logs[0].args.executor
+    this.executor = await Executor.at(executorAddr)
   })
 
-  describe('addJob', () => {
+  describe('#addJob', () => {
     it('reverts if the target is not a contract', async () => {
       await expectRevert(
         this.registry.addJob(
@@ -96,7 +107,7 @@ contract('Registry', (accounts) => {
     })
   })
 
-  describe('addFunds', () => {
+  describe('#addFunds', () => {
     let executorAddr
 
     beforeEach(async () => {
@@ -122,6 +133,132 @@ contract('Registry', (accounts) => {
       await this.registry.addFunds(executorAddr, ether('1'), { from: user1 })
       const job = await this.registry.jobs(executorAddr)
       assert.isTrue(ether('1').eq(job.balance))
+    })
+  })
+
+  describe('#executeJob', () => {
+    it('reverts if the job is not funded', async () => {
+      await expectRevert(
+        this.executor.execute("0x"),
+        '!canExecute'
+      )
+    })
+
+    context('when the job is funded', () => {
+      beforeEach(async () => {
+        await this.link.approve(this.registry.address, ether('100'), { from: maintainer })
+        await this.registry.addFunds(this.executor.address, ether('100'), { from: maintainer })
+      })
+
+      it('reverts if the target cannot execute', async () => {
+        const dummyResponse = await this.dummy.query.call("0x")
+        assert.isFalse(dummyResponse.callable)
+        await expectRevert(
+          this.executor.execute("0x"),
+          '!canExecute'
+        )
+      })
+
+      it('reverts if not enough gas supplied', async () => {
+        await this.dummy.setCanExecute(true)
+        const dummyResponse = await this.dummy.query.call("0x")
+        assert.isTrue(dummyResponse.callable)
+        assert.isTrue(await this.executor.canExecute.call())
+        await expectRevert(
+          this.executor.execute("0x", { from: user1, gas: new BN('120000') }),
+          '!gasleft'
+        )
+      })
+
+      it('executes always for the first caller if the target can execute', async () => {
+        await this.dummy.setCanExecute(true)
+        let dummyResponse = await this.dummy.query.call("0x")
+        assert.isTrue(dummyResponse.callable)
+        assert.isTrue(await this.executor.canExecute.call())
+        const balanceBefore = await this.link.balanceOf(user1)
+        const tx = await this.executor.execute("0x", { from: user1, gas: extraGas })
+        const balanceAfter = await this.link.balanceOf(user1)
+        assert.isTrue(balanceAfter.gt(balanceBefore))
+        await expectEvent.inTransaction(tx.tx, Registry, 'Executed', {
+          executor: this.executor.address,
+          target: this.dummy.address,
+          success: true
+        })
+        dummyResponse = await this.dummy.query.call("0x")
+        assert.isFalse(dummyResponse.callable)
+        assert.isFalse(await this.executor.canExecute.call())
+        const block = await web3.eth.getBlockNumber()
+        const job = await this.registry.jobs(this.executor.address)
+        assert.equal(block, job.lastExecuted)
+      })
+
+      it('pays the caller even if the target function fails', async () => {
+        const { receipt } = await this.registry.addJob(
+          this.dummy.address,
+          executeGas,
+          rewardCallers,
+          emptyBytes,
+          { from: user1 }
+        )
+        const executorAddr = receipt.logs[0].args.executor
+        const newExecutor = await Executor.at(executorAddr)
+        await this.link.approve(this.registry.address, ether('100'), { from: maintainer })
+        await this.registry.addFunds(executorAddr, ether('100'), { from: maintainer })
+        await this.dummy.setCanExecute(true)
+        const balanceBefore = await this.link.balanceOf(user1)
+        const tx = await newExecutor.execute("0x", { from: user1 })
+        const balanceAfter = await this.link.balanceOf(user1)
+        assert.isTrue(balanceAfter.gt(balanceBefore))
+      })
+
+      context('after the first successful caller', () => {
+        beforeEach(async () => {
+          await this.dummy.setCanExecute(true)
+          const dummyResponse = await this.dummy.query.call("0x")
+          assert.isTrue(dummyResponse.callable)
+          assert.isTrue(await this.executor.canExecute.call())
+        })
+
+        afterEach(async () => {
+          const dummyResponse = await this.dummy.query.call("0x")
+          assert.isFalse(dummyResponse.callable)
+          assert.isFalse(await this.executor.canExecute.call())
+        })
+
+        it('reverts if called after the first callers block', async () => {
+          await this.executor.execute("0x", { from: user1 })
+          await expectRevert(
+            this.executor.execute("0x", { from: user2 }),
+            '!canExecute'
+          )
+        })
+      })
+    })
+  })
+
+  describe('#canExecute', () => {
+    it('returns false if the job is not funded', async () => {
+      assert.isFalse(await this.executor.canExecute.call())
+    })
+
+    context('when the job is funded', () => {
+      beforeEach(async () => {
+        await this.link.approve(this.registry.address, ether('100'), { from: user1 })
+        await this.registry.addFunds(this.executor.address, ether('100'), { from: user1 })
+      })
+
+      it('returns false if the target cannot execute', async () => {
+        const dummyResponse = await this.dummy.query.call("0x")
+        assert.isFalse(dummyResponse.callable)
+        assert.isFalse(await this.executor.canExecute.call())
+      })
+
+      it('returns true if the target can execute', async () => {
+        await this.dummy.setCanExecute(true)
+        const dummyResponse = await this.dummy.query.call("0x")
+        assert.isTrue(dummyResponse.callable)
+        assert.isTrue(await this.executor.canExecute.call())
+      })
     })
   })
 })
