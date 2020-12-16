55d6144438fbdd6a389aee6019fb0ab6344f7fe6
Steve Ellis <email@steveell.is>
2020-12-16 00:45:22 -0500

    provide method or modifiers for upkeep check

--- contracts/UpkeepBase.sol
+++ contracts/UpkeepBase.sol
@@ -2,9 +2,16 @@ pragma solidity 0.6.12;
 
 contract UpkeepBase {
 
-  modifier cannotExecute()
+  function preventExecution()
+    internal
+    view
   {
     require(tx.origin == address(0), "only for simulated backend");
+  }
+
+  modifier cannotExecute()
+  {
+    preventExecution();
     _;
   }
 
