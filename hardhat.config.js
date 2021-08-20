0459c9310d8f8ec1ab779f4b42a855b2e8bcf40e
behmaroman <begmaroman@gmail.com>
2021-08-20 12:55:03 +0300

    Fixed the bug with missing argument in the constructor; Added support of Polygon Mumbai network; Updated doc to be more generalized;

--- hardhat.config.js
+++ hardhat.config.js
@@ -10,6 +10,8 @@ require("hardhat-deploy");
  */
 const kovanUrl = process.env.KOVAN_RPC_URL || 'http://localhost:8545'
 const kovanPrivateKey = process.env.KOVAN_PRIVATE_KEY || '0x00'
+const mumbaiUrl = process.env.MUMBAI_RPC_URL || 'http://localhost:8545'
+const mumbaiPrivateKey = process.env.MUMBAI_PRIVATE_KEY || '0x00'
 const deployer = process.env.DEPLOYER || 0
 
 module.exports = {
@@ -17,7 +19,13 @@ module.exports = {
     hardhat: {},
     kovan: {
       url: kovanUrl,
-      accounts: [kovanPrivateKey]
+      accounts: [kovanPrivateKey],
+      chainId: 42,
+    },
+    mumbai: {
+      url: mumbaiUrl,
+      accounts: [mumbaiPrivateKey],
+      chainId: 80001,
     }
   },
   solidity: {
@@ -33,14 +41,17 @@ module.exports = {
     linkToken: {
       1: '0x514910771AF9Ca656af840dff83E8264EcF986CA',
       42: '0xa36085F69e2889c224210F603D836748e7dC0088',
+      80001: '0x326C977E6efc84E512bB9C30f76E30c160eD06FB',
     },
     linkEth: {
       1: '0xDC530D9457755926550b59e8ECcdaE7624181557',
       42: '0x3Af8C569ab77af5230596Acf0E8c2F9351d24C38',
+      80001: '0xc0FAb0a0c9204ae4682eFdca3F05EAAb17440271', // FIXME: Replace to the real feed smart contract deployed on Mumbai. The provided one is the mock.
     },
     fastGas: {
       1: '0x169E633A2D1E6c10dD91238Ba11c4A708dfEF37C',
       42: '0x73B9b95a2AE128225dbE53A7451B6c97e3De6F08',
+      80001: '0xc0FAb0a0c9204ae4682eFdca3F05EAAb17440271', // FIXME: Replace to the real feed smart contract deployed on Mumbai. The provided one is the mock.
     },
     deployer: {
       default: deployer
